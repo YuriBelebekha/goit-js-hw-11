c89c685f594f12935d9fcc4ada3b5426ebbd19cd
Yuri Belebekha <ybelebekha@gmail.com>
2023-01-01 23:53:43 +0200

    intermediate result of the task

---
+++ src/fetchPhotos.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+export { fetchPhotos };
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY  = '32534348-8cad927330b21d66d879b216b';
+
+const { image_type, orientation, safesearch } = {  
+  'image_type':  'photo',
+  'orientation': 'horizontal',
+  'safesearch':  true,
+};
+
+async function fetchPhotos(q) {
+  try {
+    const response = await axios.get(`
+    ${BASE_URL}?key=${API_KEY}&q=${q}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}
+    `);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  };  
+};
+
+
+
+
+// function fetchPhotos(q) {
+//   return fetch(`
+//     ${BASE_URL}?key=${API_KEY}&q=${q}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}`)
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error(response.statusText);
+//         };
+//         return response.json();
+//       })
+//       .catch(error => console.error(error));
+// };
---
+++ src/index.js
@@ -0,0 +1,79 @@
+import './css/styles.css';
+import { fetchPhotos } from "./fetchPhotos";
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const refs = {
+  searchFormBtn: document.querySelector('#search-form'),
+  galleryPhoto: document.querySelector('.gallery'),
+};
+
+refs.searchFormBtn.addEventListener('submit', onSearchFormSubmit);
+
+function onSearchFormSubmit(e) {
+  e.preventDefault();
+
+  const normalizedSearchQuery = e.currentTarget.searchQuery.value.trim();
+   
+  if (normalizedSearchQuery === '') {
+    Notify.info('Your query is empty, please enter data to search.');    
+    return;
+  };  
+
+  fetchPhotos(normalizedSearchQuery)
+    .then(({ hits, totalHits } ) => {
+      if (totalHits === 0) {
+        return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      };
+
+      if (totalHits > 0) {
+        // console.log(hits); // delete
+        clearPhotoGalleryMarkup();        
+        Notify.success(`Hooray! We found ${totalHits} images.`);
+        createMarkupForPhotoGallery(hits);
+        return;
+      };
+    })
+};
+
+function createMarkupForPhotoGallery(photos) {
+  const markup = photos.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+    return `
+      <div class="photo-card">
+        <a href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+        </a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes ${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views ${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments ${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads ${downloads}</b>
+          </p>
+        </div>
+      </div>
+    `
+  }).join('');
+
+  refs.galleryPhoto.insertAdjacentHTML('beforeend', markup);
+
+  new SimpleLightbox('.gallery a');
+};
+
+function clearPhotoGalleryMarkup() {
+  refs.galleryPhoto.innerHTML = '';
+};
+
+
+
+// add refresh() method!!!!!!!!!!!!!!!
