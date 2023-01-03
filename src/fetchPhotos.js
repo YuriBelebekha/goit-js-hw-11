2985bf7b395f5e0e669305e0a5a004bcf81ec400
Yuri Belebekha <ybelebekha@gmail.com>
2023-01-03 02:37:30 +0200

    added pagination

--- src/fetchPhotos.js
+++ src/fetchPhotos.js
@@ -5,21 +5,18 @@ export { fetchPhotos };
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY  = '32534348-8cad927330b21d66d879b216b';
 
-const { image_type, orientation, safesearch } = {  
+export const { image_type, orientation, safesearch, per_page } = {  
   'image_type':  'photo',
   'orientation': 'horizontal',
-  'safesearch':  true,
+  'safesearch': true,
+  'per_page': '40', // CHANGE TO 40!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 };
 
-async function fetchPhotos(q) {
-  try {
-    const response = await axios.get(`
-    ${BASE_URL}?key=${API_KEY}&q=${q}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}
-    `);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  };  
+async function fetchPhotos(q, page) {
+  const response = await axios.get(`
+  ${BASE_URL}?key=${API_KEY}&q=${q}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}&per_page=${per_page}&page=${page}
+  `);  
+  return await response.data;
 };
 
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import { fetchPhotos } from "./fetchPhotos";
+import { per_page } from "./fetchPhotos";
 
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
@@ -8,34 +9,51 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const refs = {
   searchFormBtn: document.querySelector('#search-form'),
   galleryPhoto: document.querySelector('.gallery'),
+  guard: document.querySelector('.js-guard'),
 };
+
+let normalizedSearchQuery = '';
+let page = 1;
+
+const options = {
+  root: null,
+  rootMargin: '200px',
+  threshold: 1.0,
+};
+const observer = new IntersectionObserver(onLoad, options);
 
 refs.searchFormBtn.addEventListener('submit', onSearchFormSubmit);
 
 function onSearchFormSubmit(e) {
   e.preventDefault();
 
-  const normalizedSearchQuery = e.currentTarget.searchQuery.value.trim();
+  normalizedSearchQuery = e.currentTarget.searchQuery.value.trim();
    
   if (normalizedSearchQuery === '') {
     Notify.info('Your query is empty, please enter data to search.');    
     return;
   };  
 
-  fetchPhotos(normalizedSearchQuery)
-    .then(({ hits, totalHits } ) => {
+  fetchPhotos(normalizedSearchQuery, page)
+    .then(({ hits, totalHits }) => {
       if (totalHits === 0) {
         return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
       };
 
       if (totalHits > 0) {
         // console.log(hits); // delete
-        clearPhotoGalleryMarkup();        
+        clearPhotoGalleryMarkup();
         Notify.success(`Hooray! We found ${totalHits} images.`);
         createMarkupForPhotoGallery(hits);
+
+        // new SimpleLightbox('.gallery a'); ///////////////////////////////////
+        ///
+        observer.observe(refs.guard);
+        ///        
         return;
       };
     })
+    .catch(error => console.error(error));
 };
 
 function createMarkupForPhotoGallery(photos) {
@@ -43,7 +61,7 @@ function createMarkupForPhotoGallery(photos) {
     return `
       <div class="photo-card">
         <a href="${largeImageURL}">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" width="40"/>
         </a>
         <div class="info">
           <p class="info-item">
@@ -65,10 +83,33 @@ function createMarkupForPhotoGallery(photos) {
 
   refs.galleryPhoto.insertAdjacentHTML('beforeend', markup);
 
-  const lightbox = new SimpleLightbox('.gallery a');
-  lightbox.refresh();
+  // const lightbox = new SimpleLightbox('.gallery a');
+  // lightbox.refresh();
 };
 
 function clearPhotoGalleryMarkup() {
   refs.galleryPhoto.innerHTML = '';
+};
+
+function onLoad(entries, observer) {
+  console.log(entries);
+
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      page += 1;
+      
+      fetchPhotos(normalizedSearchQuery, page).then(data => {       
+        const totalPages = Math.ceil( data.totalHits / per_page);
+        
+        createMarkupForPhotoGallery(data.hits);
+        // const lightbox = new SimpleLightbox('.gallery a'); ///
+        //////////////////////////////// ЩОСЬ ТРЕБА РОБИТИ З РЕФРЕШ
+        // new SimpleLightbox('.gallery a');
+        
+        if (page === totalPages) {
+          observer.unobserve(refs.guard);
+        };        
+      });
+    };
+  });
 };
